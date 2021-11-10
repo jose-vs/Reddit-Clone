e42f5e31ead057161f7ed1c5fa545c0702b743d3
Jose Santos <67150374+jose-vs@users.noreply.github.com>
2021-11-11 10:07:16 +1300

    added username to navbar when logged in

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,3 +1,10 @@
-const Index = () => <div>hello world</div>;
+import { NavBar } from "../components/NavBar";
+
+const Index = () => (
+    <>
+        <NavBar/>
+        <div>ghello</div>
+    </>
+);
 
 export default Index;
